50949f418dbf2564cb4c284ce3d52f50279decfb
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-05-21 13:59:42 -0600

    consolidate to one file

--- web/wk5/index.php
+++ web/wk5/index.php
@@ -1,11 +1,29 @@
 <?php
-
-foreach ($db->query('SELECT username, password FROM note_user') as $row)
+try
 {
-  echo 'user: ' . $row['username'];
-  echo ' password: ' . $row['password'];
-  echo '<br/>';
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+?>
+
+<?php
+
 
 // $statement = $db->query('SELECT username, password FROM note_user');
 // while ($row = $statement->fetch(PDO::FETCH_ASSOC))
@@ -14,7 +32,7 @@ foreach ($db->query('SELECT username, password FROM note_user') as $row)
 // }
 
 // $statement = $db->query('SELECT username, password FROM note_user');
-// $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    // $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 // $stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
 // $stmt->bindValue(':id', $id, PDO::PARAM_INT);
@@ -27,3 +45,25 @@ foreach ($db->query('SELECT username, password FROM note_user') as $row)
 // $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>CS 313</title>
+    <link rel="stylesheet" media="screen" href="style.css" />
+  </head>
+  <body>
+    <header>
+    </header>
+    
+    <?php
+    foreach ($db->query('SELECT username, password FROM note_user') as $row)
+{
+  echo 'user: ' . $row['username'];
+  echo ' password: ' . $row['password'];
+  echo '<br/>';
+}
+?>
+
+  </body>
+</html>

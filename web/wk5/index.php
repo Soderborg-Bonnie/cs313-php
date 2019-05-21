27dff016a1865a49b19c2cb7cfe88220dab4ac0f
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-05-21 14:02:56 -0600

    more queries

--- web/wk5/index.php
+++ web/wk5/index.php
@@ -22,29 +22,6 @@ catch (PDOException $ex)
 }
 ?>
 
-<?php
-
-
-// $statement = $db->query('SELECT username, password FROM note_user');
-// while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-// {
-//   echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
-// }
-
-// $statement = $db->query('SELECT username, password FROM note_user');
-    // $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-// $stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
-// $stmt->bindValue(':id', $id, PDO::PARAM_INT);
-// $stmt->bindValue(':name', $name, PDO::PARAM_STR);
-// $stmt->execute();
-// $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-// $stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
-// $stmt->execute(array(':name' => $name, ':id' => $id));
-// $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -62,6 +39,25 @@ catch (PDOException $ex)
   echo 'user: ' . $row['username'];
   echo ' password: ' . $row['password'];
   echo '<br/>';
+  $statement = $db->query('SELECT username, password FROM note_user');
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+  echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
+}
+
+$statement = $db->query('SELECT username, password FROM note_user');
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
+$stmt->execute(array(':name' => $name, ':id' => $id));
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 }
 ?>
 

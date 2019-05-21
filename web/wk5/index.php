6b584584a00e254bd1310fa2fc5c614194764e17
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-05-21 13:44:47 -0600

    comment out code to test

--- web/wk5/index.php
+++ web/wk5/index.php
@@ -7,23 +7,23 @@ foreach ($db->query('SELECT username, password FROM note_user') as $row)
   echo '<br/>';
 }
 
-$statement = $db->query('SELECT username, password FROM note_user');
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-  echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
-}
+// $statement = $db->query('SELECT username, password FROM note_user');
+// while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+// {
+//   echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
+// }
 
-$statement = $db->query('SELECT username, password FROM note_user');
-$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+// $statement = $db->query('SELECT username, password FROM note_user');
+// $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-$stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
-$stmt->bindValue(':id', $id, PDO::PARAM_INT);
-$stmt->bindValue(':name', $name, PDO::PARAM_STR);
-$stmt->execute();
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// $stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
+// $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+// $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+// $stmt->execute();
+// $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
-$stmt->execute(array(':name' => $name, ':id' => $id));
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// $stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
+// $stmt->execute(array(':name' => $name, ':id' => $id));
+// $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>

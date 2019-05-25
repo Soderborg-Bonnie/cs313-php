a4b570b33f6626393a5f0774ddfd9843d1eb7065
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-05-25 16:38:14 -0600

    sql

--- web/wk5/details.php
+++ web/wk5/details.php
@@ -42,6 +42,8 @@ session_start();
 //     echo '<p>'.$row['book_number'].'</p>';
 // }
 // echo '<p>ugh</p>';
+
+$book_number = $_GET['book_number'];
 foreach ($db->query('SELECT * FROM isbn WHERE book_number='.$book_number) as $row)
 
 // $db->query('SELECT * FROM isbn WHERE book_number='.$book_number) as $row;

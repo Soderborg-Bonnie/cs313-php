7ef6de08987ce90741bbbf1cc0b498dc9c7c9ecb
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-05-25 16:54:34 -0600

    sql

--- web/wk5/details.php
+++ web/wk5/details.php
@@ -33,25 +33,25 @@ session_start();
 <?php
 
 
-$book_number = $_GET['book_number'];
-$sql = 'SELECT * FROM isbn WHERE book_number='.$book_number;
-$statement = $db->query($sql);
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-    echo '<p>Author:</p>';
-    echo '<p>'.$row['book_number'].'</p>';
-}
-echo '<p>ugh</p>';
-
 // $book_number = $_GET['book_number'];
-// echo $book_number;
-// foreach ($db->query('SELECT * FROM isbn WHERE book_number='.$book_number) as $row){
-
-// // $db->query('SELECT * FROM isbn WHERE book_number='.$book_number) as $row;
-//     echo '<p>Author: </p>';
+// $sql = 'SELECT * FROM isbn WHERE book_number='.$book_number;
+// $statement = $db->query($sql);
+// while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+// {
+//     echo '<p>Author:</p>';
 //     echo '<p>'.$row['book_number'].'</p>';
-//     echo $book_number;
 // }
+// echo '<p>ugh</p>';
+
+$book_number = $_GET['book_number'];
+echo $book_number;
+foreach ($db->query('SELECT book_title FROM isbn WHERE book_number='.$book_number) as $row){
+
+// $db->query('SELECT * FROM isbn WHERE book_number='.$book_number) as $row;
+    echo '<p>Author: </p>';
+    echo '<p>'.$row['book_number'].'</p>';
+    echo $book_number;
+}
 // ?> 
     </main>
 

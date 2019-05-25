1f090a7ec7caa96ce34eb5a668976cdb13803295
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-05-25 16:59:53 -0600

    sql

--- web/wk5/details.php
+++ web/wk5/details.php
@@ -45,12 +45,13 @@ session_start();
 
 $book_number = $_GET['book_number'];
 echo $book_number;
-foreach ($db->query('SELECT book_title FROM isbn WHERE book_number='.$book_number) as $row){
+// foreach ($db->query('SELECT book_title FROM isbn WHERE book_number='.$book_number) as $row){
+    foreach ($db->query('SELECT book_title FROM isbn WHERE book_number="B008TT5Q1M"') as $row){
 
 // $db->query('SELECT * FROM isbn WHERE book_number='.$book_number) as $row;
     echo '<p>Author: </p>';
-    echo '<p>'.$row['book_number'].'</p>';
-    echo $book_number;
+    echo '<p>'.$row['book_title'].'</p>';
+    // echo $book_number;
 }
 // ?> 
     </main>

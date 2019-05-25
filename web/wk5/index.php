131177048b291e319255b3112c6f26f1678fe8c5
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-05-25 11:27:37 -0600

    datatable links

--- web/wk5/index.php
+++ web/wk5/index.php
@@ -21,19 +21,9 @@ include 'connections.php';
     />
     <link rel="stylesheet" media="screen" href="../style.css" />
     <script
-      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"
-      crossorigin="anonymous"
-    ></script>
-
-    <script
-      src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"
-      crossorigin="anonymous"
-    ></script>
+		<script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
+		<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+		<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 
     <script
       src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"

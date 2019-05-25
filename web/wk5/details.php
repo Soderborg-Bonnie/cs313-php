67dff26751b31c02ba4592581ca005f499daaff1
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-05-25 16:03:22 -0600

    php

--- web/wk5/details.php
+++ web/wk5/details.php
@@ -32,7 +32,7 @@ session_start();
       
 <?php
 $book_number = $_GET['book_number'];
-$db->query('SELECT * FROM isbn WHERE isbn.book_number =' $book_number as $row);
+$db->query('SELECT * FROM isbn WHERE isbn.book_number =' .$book_number as $row);
     echo '<p>Author: </p>';
     echo '<p>'.$row['book_number'].'</p>';
     // echo $book_number;

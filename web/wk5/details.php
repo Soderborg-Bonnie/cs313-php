024e2db12746366914cde813580c6cefaefa2005
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-05-25 16:31:53 -0600

    sql

--- web/wk5/details.php
+++ web/wk5/details.php
@@ -36,11 +36,12 @@ session_start();
 $book_number = $_GET['book_number'];
 $sql = 'SELECT * FROM isbn WHERE book_number='.$book_number;
 $statement = $db->query($sql);
-$row = $statement->fetch(PDO::FETCH_ASSOC)
-
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
     echo '<p>Author:</p>';
     echo '<p>'.$row['book_number'].'</p>';
-
+}
+echo 'ugh';
 // $db->query('SELECT * FROM isbn WHERE book_number='.$book_number) as $row;
 //     echo '<p>Author: </p>';
 //     echo '<p>'.$row['book_number'].'</p>';

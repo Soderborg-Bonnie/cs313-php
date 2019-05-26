b4194202bd6dc3f36f28fe195978bb7b2a3eecde
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-05-25 18:01:54 -0600

    sql

--- web/wk5/index.php
+++ web/wk5/index.php
@@ -78,17 +78,13 @@ session_start();
         <tbody>
         <?php 
 
-          // foreach ($db->query('SELECT book_title, author_id, book_number FROM isbn INNER JOIN author WHERE isbn.book_number = author.book_number LIMIT 10') as $row)
-          foreach ($db->query('SELECT * FROM isbn, author WHERE isbn.book_number = author.book_number LIMIT 10') as $row)
+          foreach ($db->query('SELECT * FROM isbn, author WHERE isbn.book_number = author.book_number LIMIT 10') as $row){
           // book_title, author_id, book_number
           // INNER JOIN author WHERE isbn.book_number = author.book_number
-            { 
+             
               echo '<tr>';  
               // echo '<td><a href="../details.php/details.php">'.$row['book_title'].'</a></td>';
-              echo '<td><a href="../details.php/details.php?book_number= '.$row['book_number'].'">'.$row['book_title'].'</a></td>';
-
-              $_SESSION['book_number']=$row['book_number'];
-
+              echo '<td><a href="../details.php/details.php?book_number=" '.$row['book_number'].'>'.$row['book_title'].'</a></td>';
               echo '<td>'.$row['author_name'].'</td>';
               // echo '<td>'.''.'</td>';
               // echo '<td>'.''.'</td>';
@@ -96,7 +92,7 @@ session_start();
               // echo '<td>'.''.'</td>';
               echo '<td>'.$row['book_number'].'</td>';
               echo '</tr>';
-            }
+          }
 
         ?> 
         </tbody>

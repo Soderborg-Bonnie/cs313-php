18947152cb71b2c2c5713297d2629a83719df399
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-05-28 19:45:08 -0600

    team

--- web/wk6/teamActivity/addScriptures.php
+++ web/wk6/teamActivity/addScriptures.php
@@ -15,19 +15,43 @@ session_start();
     <form>
         <label>Book: 
             <input type="text">
-        </label>
+        </label><br>
         <label>Chapter: 
             <input type="text">
-        </label>
+        </label><br>
         <label>Verse: 
             <input type="text">
-        </label>
+        </label><br>
         <label>Content: 
             <input type="text">
-        </label>
+        </label><br>
+        
+        <?php
+            foreach ($db->query('SELECT * FROM topic') as $row)
+            <input type="checkbox" name="topic" value="topic"><br>
+        ?>
+         
+        
+
     </form>
 </main>
 
 
 </body>
 </html>
+<?php 
+
+          foreach ($db->query('SELECT * FROM isbn, author WHERE isbn.book_number = author.book_number') as $row)
+            { 
+              echo '<tr>';  
+              echo '<td><a href="../details.php/details.php?book_number='.$row['book_number'].'">'.$row['book_title'].'</a></td>';
+              echo '<td>'.$row['author_name'].'</td>';
+              // echo '<td>'.''.'</td>';
+              // echo '<td>'.''.'</td>';
+              // echo '<td>'.''.'</td>';
+              // echo '<td>'.''.'</td>';
+              echo '<td>'.$row['book_number'].'</td>';
+              echo '</tr>';
+            }
+            $_SESSION['book_number']=$row['book_number'];
+        ?>

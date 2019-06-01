9aced238b6335cb8725bcd44dde34c70e1cb2f18
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-06-01 14:20:59 -0600

    additions

--- web/wk6/additions.php
+++ web/wk6/additions.php
@@ -72,7 +72,9 @@ session_start();
 
 ?>
 
-      <form action="/wk6/additions.php/additions.php" method="POST">
+      <!-- <form action="/wk6/additions.php/additions.php" method="POST"> -->
+      <form action="additions.php" method="POST">
+
                             <h2>Add a book</h2><br /> 
                             <div id="inputForm">                           
                             <label>Title:  </label>

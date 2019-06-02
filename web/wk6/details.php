d531eae43fe37c8f9541c3ea7f287799456462bc
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-06-01 19:54:53 -0600

    additions

--- web/wk6/details.php
+++ web/wk6/details.php
@@ -64,7 +64,7 @@ $book_number = $_GET['book_number'];
     foreach ($db->query("SELECT * FROM media WHERE book_number='$book_number'") as $row4){
     foreach ($db->query("SELECT * FROM genre WHERE book_number='$book_number'") as $row5){
     foreach ($db->query("SELECT * FROM tags WHERE book_number='$book_number'") as $row6){
-    echo '<form action="../index.php/index.php" method="POST">';
+    echo '<form action="../successfulDelete.php/successfulDelete.php" method="POST">';
     echo '<h2>Details</h2><br />';
     echo '<label>Title:  </label>';
     echo '<input type="text" name="title" id="title" value="'.$row['book_title'].'"/><br />';

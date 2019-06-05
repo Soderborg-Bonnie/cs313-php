744524c9f75052d6d37321c520a5178724e8b665
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-06-05 13:08:30 -0600

    registration

--- web/wk7/registration.php
+++ web/wk7/registration.php
@@ -5,7 +5,7 @@ require 'connections.php';
 session_start();
             $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
             $password_clearText = filter_input(INPUT_POST, 'pwd', FILTER_SANITIZE_STRING);
-            $userError = $pwdError = '';
+            $usernameError = $pwdError = '';
 
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 if (strlen($password_clearText)>=8
@@ -59,11 +59,12 @@ session_start();
               }
 ?> 
 
-<!DOCTYPE html>
-<html lang="en">
+<!-- <!DOCTYPE html>
+<html lang="en"> -->
   <head>
     <meta charset="utf-8" />
     <title>registration</title>
+    <link rel="stylesheet" media="screen" href="../style.css" />
     <link
       rel="stylesheet"
       href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
@@ -74,7 +75,26 @@ session_start();
       rel="stylesheet"
       href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css"
     />
-    <link rel="stylesheet" media="screen" href="../style.css" />
+  </head>
+  <body id ="registrationBody">
+    <main>
+    <h1 id="registrationTitle">Finding Books</h1><br><br>
+        <!-- <img src="../images/sparklyBook.jpg" alt="sparkly book" id="loginPic"/> -->
+    <!-- Can Stock Photography by Jag_cz https://www.canstockphoto.com/old-book-on-wooden-table-22417225.html -->
+      <!-- <h2>Table o' Books</h2> -->
+      <form action="../login.php/login.php" method="POST">
+        <div id="registrationInput">
+            <h2>Welcome! Please <a href="../login.php/login.php">login</a> or register.</h2><br><br>
+            <label>Username: <span class="error">* <?php echo $userError; ?></span></label>
+            <input type="text" name="username" placeholder="username" required="required"><br>
+            <label>Password: <span class="error">* <?php echo $pwdError; ?></span></label>
+            <input type="password" name="pwd" id="password"  placeholder="password" required="required" pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"><br>  
+            <h6>Password should be at least 8 characters long and include at least 1 number.</h6> <br><br>    
+            <button type="submit" class="btn btn-success" name="register" value="register" id="register">register</button>
+        </div>
+      </form>
+    </main>
+    <script src="../main.js"></script>
     <script
 		<script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
 		<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
@@ -90,27 +110,5 @@ session_start();
       integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
       crossorigin="anonymous"
     ></script>
-    <script
-      src="../main.js"
-    ></script>
-  </head>
-  <body id ="registrationBody">
-    <main>
-    <h1 id="registrationTitle">Finding Books</h1><br><br>
-        <!-- <img src="../images/sparklyBook.jpg" alt="sparkly book" id="loginPic"/> -->
-    <!-- Can Stock Photography by Jag_cz https://www.canstockphoto.com/old-book-on-wooden-table-22417225.html -->
-      <!-- <h2>Table o' Books</h2> -->
-      <form action="../login.php/login.php" method="POST">
-        <div id="registrationInput">
-            <h2>Welcome! Please <a href="../login.php/login.php">login</a> or register.</h2><br><br>
-            <label>Username: <span class="error">* <?php echo $userError; ?></span></label>
-            <input type="text" name="username" placeholder="username" required="required"><br>
-            <label>Password: <span class="error">* <?php echo $pwdError; ?></span></label>
-            <input type="password" name="pwd" id="password"  placeholder="password" required="required" pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"><br>  
-            <h6>Password should be at least 8 characters long and include at least one number.</h6> <br><br>    
-            <button type="submit" class="btn btn-success" name="register" value="register" id="register">register</button>
-        </div>
-      </form>
-    </main>
   </body>
-</html>
+<!-- </html> -->

2fe03df0f5a61d6518557b9db094e34d64933e1a
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-06-05 10:43:56 -0600

    registration

--- web/wk7/registration.php
+++ web/wk7/registration.php
@@ -70,7 +70,7 @@ session_start();
             <label>Password: 
                 <input type="password">
             </label><br><br>
-            <input type="submit" class="btn btn-success" name="loginSubmit" value="loginSubmit" id="loginSubmit">
+            <input type="submit" class="btn btn-success" name="Register" value="register" id="register">
         </div>
     </main>
   </body>

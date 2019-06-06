f2d42c9b95fa3892075959482420ebfdb22c9f55
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-06-06 16:45:24 -0600

    pics

--- web/wk7/main.js
+++ web/wk7/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
     $('#bookTable').DataTable();
 } );
+function changePic7back(){
+    document.getElementById('topImage').style.transitionProperty = 'opacity';
+    document.getElementById('topImage').style.opacity='0';
+    document.getElementById('topImage').style.transitionDuration = '.5s';
+}
 // onload document.getElementById('topImage');

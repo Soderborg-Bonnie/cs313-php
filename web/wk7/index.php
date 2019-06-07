28304452a0b5e6b6163cdb307f0b59e77c8ea5e3
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-06-06 23:18:18 -0600

    reg

--- web/wk7/index.php
+++ web/wk7/index.php
@@ -73,6 +73,7 @@ $GLOBALS['conn']=$db;
             }
         }
         function regUser($username, $password_clearText) {
+          try{
             $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
             $username = test_input($username);
             $password_clearText = test_input($password_clearText);
@@ -84,6 +85,10 @@ $GLOBALS['conn']=$db;
             $rowsChanged = $statement->rowCount();
             $statement->closeCursor();
             return $rowsChanged;
+          }
+          catch{
+            return 0; 
+          }
         }
         function test_input($data) {
             $data = trim($data);
@@ -93,22 +98,5 @@ $GLOBALS['conn']=$db;
             }
 
             ?>
-
-    <!-- <script src="../main.js"></script>
-    <script
-		<script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
-		<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-		<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
-
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-      crossorigin="anonymous"
-    ></script> -->
   </body>
 </html>

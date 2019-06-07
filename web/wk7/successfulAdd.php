d6eb6e87f5a1259106f96de116a1d1536430b3df
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-06-07 15:11:20 -0600

    reg

--- web/wk7/successfulAdd.php
+++ web/wk7/successfulAdd.php
@@ -82,18 +82,18 @@ session_start();
                         }
 
                     function checkBook($isbn) {
-                      $sql = 'SELECT book_number FROM isbn WHERE book_number = :isbn';
+                      $sql = 'SELECT book_number FROM isbn WHERE :isbn = book_number';
                       echo 'records: '.$sql;
                       $stmt = $GLOBALS['conn']->prepare($sql);
-                      echo 'prepare: '.$stmt;
+                      // echo 'prepare: '.$stmt;
                       $stmt->bindValue(':isbn', $isbn, PDO::PARAM_STR);
-                      echo 'bind: '.$stmt;
+                      // echo 'bind: '.$stmt;
                       $stmt->execute();
-                      echo 'execute: '.$stmt;
+                      // echo 'execute: '.$stmt;
                       $results = $stmt->fetch(PDO::FETCH_ASSOC);
-                      echo 'results: '.$results;
+                      // echo 'results: '.$results;
                       $stmt->closeCursor();
-                      echo 'close: '.$stmt;
+                      // echo 'close: '.$stmt;
                       if (!is_array($results)) {
                         echo '<h2>Your addition of a new book was successful!</h2><br><br>';
                         echo "'$title' has been added."."<br>";

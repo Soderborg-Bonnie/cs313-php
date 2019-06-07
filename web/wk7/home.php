515e6f318f19666b7dc6316e0f2ff325d2b24f33
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-06-06 22:37:26 -0600

    scripts

--- web/wk7/home.php
+++ web/wk7/home.php
@@ -12,7 +12,8 @@ if (isset($_SESSION['username']))
 else
 {
 	header("Location: ../login.php/login.php");
-	die(); 
+	die(); // we always include a die after redirects.
+}
 ?> 
 
 <!DOCTYPE html>
@@ -31,7 +32,6 @@ else
       href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css"
     />
     <link rel="stylesheet" media="screen" href="../style.css" />
-    <script src="../main.js"></script>
     <script
 		<script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
 		<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
@@ -47,8 +47,14 @@ else
       integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
       crossorigin="anonymous"
     ></script>
+    <script
+      src="../main.js"
+    ></script>
   </head>
   <body id="homeBody">
+    <!-- <div>
+        <img src="../images/sparklyBook.jpg" alt="sparkly book" id="topImage">
+    </div> -->
     <header>
       <div class="topOfPage">
         <img src="../images/sparklyBookSm.jpg" alt="sparkly books" />
@@ -57,6 +63,7 @@ else
           <nav>
             <ul class="mainNav">
               <li><a href="../home.php/home.php" class="current">Home</a></li>
+              <!-- <li><a href="../details.php/details.php" >Details</a></li> -->
               <li><a href="../logout.php/logout.php">Logout</a></li>
             </ul>
           </nav>
@@ -84,7 +91,13 @@ else
         </thead>
         <tbody>
         <?php 
+
           foreach ($db->query('SELECT * FROM isbn, author, words, media, genre, tags WHERE isbn.book_number = author.book_number AND isbn.book_number = words.book_number AND isbn.book_number = media.book_number AND isbn.book_number = genre.book_number AND isbn.book_number = tags.book_number') as $row)
+            // foreach ($db->query("SELECT * FROM isbn, words WHERE book_number='$book_number'") as $row2){
+            // foreach ($db->query("SELECT * FROM isbn, media WHERE book_number='$book_number'") as $row3){
+            // foreach ($db->query("SELECT * FROM isbn, genre WHERE book_number='$book_number'") as $row4){
+            // foreach ($db->query("SELECT * FROM isbn, tags WHERE book_number='$book_number'") as $row5){
+
             { 
               echo '<tr>';  
               echo '<td><a href="../details.php/details.php?book_number='.$row['book_number'].'">'.$row['book_title'].'</a></td>';
@@ -96,28 +109,13 @@ else
               echo '<td class="tableDisplay">'.$row['book_number'].'</td>';
               echo '</tr>';
             }
+        // }}}}
             $_SESSION['book_number']=$row['book_number'];
         ?> 
         </tbody>
       </table>
     </main>
     <footer>&copy 2019 Bonnie Soderborg All rights reserved.</footer>
-    <!-- <script src="../main.js"></script>
-    <script
-		<script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
-		<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-		<script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
-
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-      integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-      integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-      crossorigin="anonymous"
-    ></script> -->
   </body>
 </html>
 

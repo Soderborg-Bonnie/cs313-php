658ed5f8cc587d8d7ae9285d7136163e575e4272
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-06-07 00:10:49 -0600

    reg

--- web/wk7/index.php
+++ web/wk7/index.php
@@ -56,7 +56,7 @@ $GLOBALS['conn']=$db;
             {
                 $logins = checkUser($username);
                 if ($logins = 1){
-                  echo '<p class="error">***Error! Try a different username.</p>';
+                  echo '<h3 class="error">***Error! Try a different username.</h3>';
                 }else{
                 $rows = regUser($username, $password_clearText);
                 if ($rows > 0)

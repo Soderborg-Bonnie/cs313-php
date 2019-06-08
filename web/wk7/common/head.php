634d0584cb25594db9c75ee4156e760443cbefba
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-06-08 10:59:02 -0600

    clean up

---
+++ web/wk7/common/head.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="utf-8" />
+    <title>Add a book</title>
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css"
+    />
+    <link rel="stylesheet" media="screen" href="../style.css" />
+  </head>

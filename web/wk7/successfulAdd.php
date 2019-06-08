6103b24f7f0941f8ee835ddab2ec6836fce50865
Soderborg-Bonnie <38639336+Soderborg-Bonnie@users.noreply.github.com>
2019-06-08 12:56:39 -0600

    add book

--- web/wk7/successfulAdd.php
+++ web/wk7/successfulAdd.php
@@ -1,16 +1,14 @@
 <?php 
-
-$GLOBALS['book_number']='book_number';
-
-//Get the database connection file  
-require 'connections.php';  
-session_start();
-$GLOBALS['conn']=$db;
-
+  $GLOBALS['book_number']='book_number';
+  //Get the database connection file  
+  require 'connections.php';  
+  session_start();
+  $GLOBALS['conn']=$db;
 ?> 
 
 <!DOCTYPE html>
 <html lang="en">
+<!-- use own css, bootstrap and datatable libraries------------------------------------------------------ -->
   <head>
     <meta charset="utf-8" />
     <title>Success!</title>
@@ -33,81 +31,74 @@ $GLOBALS['conn']=$db;
         </div>
       </div>
     </header>
-    <!-- <h2>Your addition of a new book was successful!</h2><br><br> -->
+<!-- get input data--------------------------------------------------------------------------------------- -->
     <?php
-                        $title = ($_POST['title']);
-                        $isbn = ($_POST['isbn']);
-                        $author = ($_POST['author']);
-                        $words = ($_POST['words']);
-                        $media = ($_POST['media']);
-                        $genre = ($_POST['genre']);
-                        $tags = ($_POST['tags']);
-                        // $author_id =  ($_POST['author_id']);
-                        // echo "'$title' has been added."."<br>";
-                        // echo "It's ISBN number is: $isbn";
+      $title = ($_POST['title']);
+      $isbn = ($_POST['isbn']);
+      $author = ($_POST['author']);
+      $words = ($_POST['words']);
+      $media = ($_POST['media']);
+      $genre = ($_POST['genre']);
+      $tags = ($_POST['tags']);
 
-                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                          $books = checkBook($isbn);
-                          // echo 'books: '.$books;
-                          if ($books == 0){
-                            
-                          
-                          $statement = $db->prepare("INSERT INTO isbn (book_number, book_title) VALUES (:isbn, :title)");
-                          $statement->bindValue(':isbn', $isbn);
-                          $statement->bindValue(':title', $title);
-                          $statement->execute();
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// check if book is in database already---------------------------------------------------------------------------
+        $books = checkBook($isbn);
+        if ($books == 0){
+// insert new book------------------------------------------------------------------------------------------------
+          $statement = $db->prepare("INSERT INTO isbn (book_number, book_title) VALUES (:isbn, :title)");
+          $statement->bindValue(':isbn', $isbn);
+          $statement->bindValue(':title', $title);
+          $statement->execute();
 
-                          $statement = $db->prepare("INSERT INTO author (book_number, author_name) VALUES (:isbn, :author)");
-                          $statement->bindValue(':isbn', $isbn);
-                          $statement->bindValue(':author', $author);
-                          $statement->execute();
+          $statement = $db->prepare("INSERT INTO author (book_number, author_name) VALUES (:isbn, :author)");
+          $statement->bindValue(':isbn', $isbn);
+          $statement->bindValue(':author', $author);
+          $statement->execute();
 
-                          $statement = $db->prepare("INSERT INTO words (book_number, words) VALUES (:isbn, :words)");
-                          $statement->bindValue(':isbn', $isbn);
-                          $statement->bindValue(':words', $words);
-                          $statement->execute();
+          $statement = $db->prepare("INSERT INTO words (book_number, words) VALUES (:isbn, :words)");
+          $statement->bindValue(':isbn', $isbn);
+          $statement->bindValue(':words', $words);
+          $statement->execute();
 
-                          $statement = $db->prepare("INSERT INTO media (book_number, media) VALUES (:isbn, :media)");
-                          $statement->bindValue(':isbn', $isbn);
-                          $statement->bindValue(':media', $media);
-                          $statement->execute();
+          $statement = $db->prepare("INSERT INTO media (book_number, media) VALUES (:isbn, :media)");
+          $statement->bindValue(':isbn', $isbn);
+          $statement->bindValue(':media', $media);
+          $statement->execute();
 
-                          $statement = $db->prepare("INSERT INTO genre (book_number, genre) VALUES (:isbn, :genre)");
-                          $statement->bindValue(':isbn', $isbn);
-                          $statement->bindValue(':genre', $genre);
-                          $statement->execute();
+          $statement = $db->prepare("INSERT INTO genre (book_number, genre) VALUES (:isbn, :genre)");
+          $statement->bindValue(':isbn', $isbn);
+          $statement->bindValue(':genre', $genre);
+          $statement->execute();
 
-                          $statement = $db->prepare("INSERT INTO tags (book_number, tags) VALUES (:isbn, :tags)");
-                          $statement->bindValue(':isbn', $isbn);
-                          $statement->bindValue(':tags', $tags);
-                          $statement->execute();
+          $statement = $db->prepare("INSERT INTO tags (book_number, tags) VALUES (:isbn, :tags)");
+          $statement->bindValue(':isbn', $isbn);
+          $statement->bindValue(':tags', $tags);
+          $statement->execute();
 
-                        }else{
-                          // echo '<h3 class="error">***Oops! That book is already in here.</h3>'; 
-                          $_SESSION['book']=$isbn;
-                          header('Location: ../additions.php/additions.php');
-                          die();
-                        }}
+        }else{
+// add session cookie------------------------------------------------------------------------------------------
+          $_SESSION['book']=$isbn;
+          header('Location: ../additions.php/additions.php');
+          die();
+        }}
 
-                    function checkBook($isbn) {
-                      $sql = 'SELECT book_number FROM isbn WHERE book_number  =:isbn ';
-                      $stmt = $GLOBALS['conn']->prepare($sql);
-                      $stmt->bindValue(':isbn', $isbn, PDO::PARAM_STR);
-                      $stmt->execute();
-                      $results = $stmt->fetch(PDO::FETCH_ASSOC);
-                      $stmt->closeCursor();
-                      if (!is_array($results)) {
-                        echo '<h2>Your addition of a new book was successful!</h2><br><br>';
-                        echo "'$title' has been added."."<br>";
-                        // echo "It's ISBN number is: $isbn";
-                        return 0;
-                      } else {
-                        echo '<h3 class="error">***Oops! That book is already in here.</h3>'; 
-                        return 1;
-                      }
-                    }
-
+        function checkBook($isbn) {
+          $sql = 'SELECT book_number FROM isbn WHERE book_number  =:isbn ';
+          $stmt = $GLOBALS['conn']->prepare($sql);
+          $stmt->bindValue(':isbn', $isbn, PDO::PARAM_STR);
+          $stmt->execute();
+          $results = $stmt->fetch(PDO::FETCH_ASSOC);
+          $stmt->closeCursor();
+          if (!is_array($results)) {
+            echo '<h2>Your addition of a new book was successful!</h2><br><br>';
+            return 0;
+          } else {
+            echo '<h3 class="error">***Oops! That book is already in here.</h3>'; 
+            return 1;
+          }
+        }
       ?>
-
-</body>
+    <footer class="fixedFooter">&copy 2019 Bonnie Soderborg All rights reserved.</footer>
+  </body>
 </html>
